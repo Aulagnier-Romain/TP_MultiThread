5788745b6358601276ee83d1316a2f5bc43899f5
Romain <Romain@DESKTOP-BGNDF0G>
2025-02-02 15:19:37 +0100

    Ajoutez des fichiers projet.

---
+++ TP_MultiThread/MainThread.cs
@@ -0,0 +1,66 @@
+﻿using System.Transactions;
+using System.IO;
+
+namespace TP_MultiThread
+{
+    internal class MainThread
+    {
+        String fichier;
+
+        static void Main(string[] args)
+        {
+            Work work;
+            Mutex m = new Mutex(true);
+
+            for (int i = 0; i < 10; i++)
+            {
+                work = new Work();
+                work.mut = m;
+                work.numero = i;
+                Thread t = new Thread(work.Run);
+                t.Start();
+            }
+
+            m.ReleaseMutex();
+            
+        }
+
+        public void Ecrire()
+        {
+            try
+            {
+                StreamWriter sw = new StreamWriter("\"D:\\Cours\\BTS2_Astier\\CS\\test.txt\"");
+                sw.WriteLine("Hello");
+                sw.WriteLine("");
+                sw.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+        public void Lire()
+        {
+            try
+            {
+                StreamReader sr = new StreamReader("\"D:\\Cours\\BTS2_Astier\\CS\\test.txt\"");
+                fichier = sr.ReadLine();
+                while (fichier != null)
+                {
+                    Console.WriteLine(fichier);
+                    fichier = sr.ReadLine();
+                }
+                sr.Close();
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+
+
+
+    }
+}
---
+++ TP_MultiThread/Work.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_MultiThread
+{
+    public class Work
+    {
+
+        public Mutex mut;
+        public int numero;
+
+        public void Run()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                mut.WaitOne();
+                Console.ForegroundColor = (ConsoleColor)(numero + 1);
+                Console.WriteLine("Run " + numero + " : " + i);
+                mut.ReleaseMutex();
+            }
+
+        }
+    }
+}
